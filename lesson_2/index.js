2b50f535f945b42341ae95ed84d52f2c3420fb10
Diana Murtazina <murka8831@yandex.ru>
2022-09-12 00:18:43 +0400

    working with tags

---
+++ lesson_2/index.js
@@ -0,0 +1,5 @@
+$("h2, p"); // Первый, второй, третий и т.д.
+$("h2 span"); // outer и inner
+$("h2 > span"); // parent > child
+$("h2 + p"); // prev + next
+$("h2 ~ p").css("border", "1px solid black"); // prev ~ next
