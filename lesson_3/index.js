981985cdd84aa6883a8e06edcbb5e75f7c761252
Diana Murtazina <murka8831@yandex.ru>
2022-09-13 13:58:05 +0400

    added homework 3

--- lesson_3/index.js
+++ lesson_3/index.js
@@ -6,3 +6,14 @@
 //$('a[href*="ya"]').css("border", "1px solid green"); //Содержит ...
 //$('a[data-target|="main"]').css("border", "1px solid green"); //Имеет префикс ...
 //$("a[href][download]").css("border", "1px solid green"); //Имеет одновременно несколько атрибутов ...
+
+// Атрибуту href
+//$("[href]").css("border", "1px solid red");
+// Атрибуту href, который полностью соответствует "http://mail.ru"
+//$("a[href='http://mail.ru']").css("border", "1px solid red");
+// Атрибуту src, который начинается на "img/"
+//$("[src^='img']").css("border", "1px solid red");
+// Атрибуту src, который заканчивается на ".js"
+//$('[src$=".js"]').css("border", "1px solid red");
+// Атрибутам src и title
+//$("[src], [title]").css("border", "1px solid red");
