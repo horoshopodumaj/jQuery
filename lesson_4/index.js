44c1966bb0793d4b5f62075972489822c74be3bf
Diana Murtazina <murka8831@yandex.ru>
2022-09-13 21:42:04 +0400

    added lesson 4

---
+++ lesson_4/index.js
@@ -0,0 +1,17 @@
+/* ====== Фильтры ===== */
+
+/* Простые фильтры в jQuery */
+
+//$("p:first").css("border", "1px solid blue"); // Первый элемент
+//$("p:last").css("border", "1px solid blue"); // Последний элемент
+//$("a:eq(1)").css("border", "1px solid green"); // Элемент под номером ... (нумерация с 0)
+//$("*:not(li, a, ul)").css("border", "1px solid yellow"); // Исключает из найденных элементов selector
+//$("li:even").css("border", "1px solid red"); // Четные найденные элементы
+//$("li:odd").css("border", "1px solid red"); // Нечетные найденные элементы
+//$("li:gt(2)").css("border", "1px solid red"); // Элементы чей порядковый номер больше n
+//$("li:lt(2)").css("border", "1px solid green"); // Элементы чей порядковый номер меньше n
+//$("*:header").css("border", "1px solid blue"); // Только те элементы, которые являются заголовками
+//$('li:animated').css("border", "1px solid blue");// Только те элементы, которые задействованы в анимации
+//$("li:hidden").css("border", "1px solid blue"); // Скрытые элементы
+//$("li:visible").css("border", "1px solid blue"); // Видимые элементы
+//$("li:lang(ru)").css("border", "1px solid blue"); // Фильтрация по языку
