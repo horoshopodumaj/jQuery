54f760fcc0f55c06d8d527c0cc4d2a7ca28e9779
Diana Murtazina <murka8831@yandex.ru>
2022-09-18 22:43:15 +0400

    added the fifth lesson

---
+++ lesson_5/index.js
@@ -0,0 +1,8 @@
+/* ====== Фильтры ===== */
+
+/* Фильтры по содержимому в jQuery */
+
+//$("p:contains(lorem)").css("border", "1px solid red"); // содержит текст
+//$("p:empty").text("Text"); // пустой
+//$("h2:has(span)").css("border", "1px solid red"); // имеет внутри селектор
+//$("span:parent(h2)").css("border", "1px solid red"); // обращение элементу у которого есть определенный родитель
