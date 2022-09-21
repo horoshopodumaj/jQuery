a66c19fb608a9aea7f3837088e0cea34e0641078
Diana Murtazina <murka8831@yandex.ru>
2022-09-22 00:02:35 +0400

    added the ninth lesson

---
+++ lesson_9/index.js
@@ -0,0 +1,21 @@
+/* ====== События браузера и страницы ===== */
+
+// $(function () {
+//     $("p").text("DOM ready");
+// }); // Готовность DOM элементов
+
+// $(window).load(function () {
+//     console.log("LOAD");
+// }); // Полная загрузка страницы
+
+// $(window).unload(function () {
+//     console.log("Bye");
+// }); // Выход со страницы
+
+// $(window).resize(function () {
+//     console.log("resize");
+// }); // Изменение размера (например, окна)
+
+// $(window).scroll(function () {
+//     console.log("Scroll");
+// }); // Прокрутка элемента
