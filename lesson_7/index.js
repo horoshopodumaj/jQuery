48e79484572593515d7d5df7edf5488d2add8564
Diana Murtazina <murka8831@yandex.ru>
2022-09-21 22:55:32 +0400

    added homework for the seventh lesson

--- lesson_7/index.js
+++ lesson_7/index.js
@@ -28,6 +28,24 @@
 //     $(this).css("color", "purple");
 // }); // Мышь покидает площадь элемента
 
-$("ul li").mousemove(function () {
-    $(this).toggleClass("blue");
-}); // Движение мыши над элементом
+// $("ul li").mousemove(function () {
+//     $(this).toggleClass("blue");
+// }); // Движение мыши над элементом
+
+// При клике на кнопку, вывести сообщение "Приветствую, пользователь!"
+
+// $("button").click(function () {
+//     console.log("Приветствую, пользователь!");
+// });
+
+// При двойном клике на элемент, присвоить ему класс active
+
+// $("ul li").dblclick(function () {
+//     $(this).addClass("active");
+// });
+
+// Сделать выпадающее меню, при наведении на элемент
+
+// $("ul li").mouseenter(function () {
+//     alert("выпадающее меню");
+// });
