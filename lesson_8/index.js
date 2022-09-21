fca1e13b3a86ad7e730165de10a72763d096f44f
Diana Murtazina <murka8831@yandex.ru>
2022-09-21 23:31:10 +0400

    added the eighth lesson

---
+++ lesson_8/index.js
@@ -0,0 +1,18 @@
+/* ====== События клавиатуры ===== */
+// $("input").keydown(function () {
+//     console.log($(this).val());
+// }); // Клавиша в нажатом состоянии
+
+// $("input").keyup(function () {
+//     console.log($(this).val());
+// });
+
+// $("input").keyup(function () {
+//     $("p").text($(this).val());
+// });
+// Клавиша больше не в нажатом состоянии
+
+// $("input").keypress(function (eventObject) {
+//     console.log(eventObject.which);
+// });
+// Ввод символа с клавиатуры
