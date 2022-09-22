1ed0bcfd0cf0bcee633c07280eb1bfaa5b22ae50
Diana Murtazina <murka8831@yandex.ru>
2022-09-22 18:02:04 +0400

    added the eleventh lesson

---
+++ lesson_11/index.js
@@ -0,0 +1,16 @@
+/* ====== Переменные и их свойства ===== */
+
+var email = "@cat",
+    name = "Mr. Cat",
+    surname = "Meow";
+
+var num = 5423; // Числа
+var str = "Можно писать что угодно"; // Строки
+var bool = false; // Булевые значения
+var myVar = null; // Ничего :)
+var undefinds; // Значение не присвоено
+var man = {
+    name: "Артем",
+    age: 25,
+    message: "привет",
+};
