f5560cb4a7f8739c1b9c08686f8b9ffda927c87c
Diana Murtazina <murka8831@yandex.ru>
2022-09-22 16:24:07 +0400

    added the tenth lesson

---
+++ lesson_10/index.js
@@ -0,0 +1,19 @@
+/* ====== Базовые события ===== */
+
+// click()
+// $("ul li").click(function () {
+//     console.log("Click");
+//     $("ul").append("<li>New element</li>");
+// });
+
+// $("ul").on("click", "li", function () {
+//     console.log("Click 2");
+//     $("ul").append("<li>New element</li>");
+// });
+
+// function addAndStop() {
+//     $("ul").append("<li>New element</li>");
+//     $("ul").off("click");
+// }
+
+// $("ul").on("click", "li", addAndStop); //удаление обработчика событий
