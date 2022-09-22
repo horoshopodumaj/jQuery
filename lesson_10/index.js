4a0f3eaa7161382cbb207741ec791c05fae310ae
Diana Murtazina <murka8831@yandex.ru>
2022-09-22 17:03:22 +0400

    added homework for the tenth lesson

--- lesson_10/index.js
+++ lesson_10/index.js
@@ -17,3 +17,21 @@
 // }
 
 // $("ul").on("click", "li", addAndStop); //удаление обработчика событий
+
+//В HTML документе есть кнопка "Создать картинку". При клике на кнопку нужно создать картинку и еще одну кнопку. На картинке может быть любое изображение, а вторая кнопка содержит текст "Удалить картинку. При клике на свежесозданную кнопку удаляется картинка и сама кнопка.
+
+// $("button").click(function () {
+//     $("div .image")
+//         .append("<p>Image</p>")
+//         .append("<button class='remove'>Удалить картинку</button>");
+// });
+
+$("button").on("click", function () {
+    $("div .image")
+        .append("<p>Image</p>")
+        .append("<button class='remove'>Удалить картинку</button>");
+});
+
+$("div .image").on("click", "button", function () {
+    $("button.remove, p").remove();
+});
