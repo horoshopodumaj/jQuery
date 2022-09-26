e0499ec074df1e8d6c6c696b4dd2fa2fe019a244
Diana Murtazina <murka8831@yandex.ru>
2022-09-26 12:53:22 +0400

    added the 19 lesson

--- lesson_19/lesson_19/js/index.js
+++ lesson_19/lesson_19/js/index.js
@@ -1,1 +1,13 @@
 /* Технология AJAX */
+
+$(function () {
+    let sectionHeight = $("#section-1").height();
+    console.log(sectionHeight);
+    $(window).scroll(function () {
+        let top = $(this).scrollTop();
+        console.log(top);
+        if (top == sectionHeight) {
+            $("#sections").load("./sections.html");
+        }
+    });
+});
